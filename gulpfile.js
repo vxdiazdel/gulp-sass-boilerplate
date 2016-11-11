730db1aa16be3216620982cfdbe31b63b4378e51
Victor Diaz de Leon <vrdiazdel@gmail.com>
2016-11-10 23:19:06 -0800

    Update npm packages and gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
-var gulp 		= require('gulp'),
-	uglify 		= require('gulp-uglify'),
-	sass 			= require('gulp-sass')
-	plumber 	= require('gulp-plumber'),
-	prefix 		= require('gulp-autoprefixer'),
-	imagemin 	= require('gulp-imagemin');
+var gulp 				= require('gulp'),
+		babel				= require('gulp-babel'),
+		concat			= require('gulp-concat'),
+		sourcemaps	= require('gulp-sourcemaps'),
+		uglify 			= require('gulp-uglify'),
+		sass 				= require('gulp-sass')
+		plumber 		= require('gulp-plumber'),
+		prefix 			= require('gulp-autoprefixer'),
+		imagemin 		= require('gulp-imagemin'),
+		browserSync	=	require('browser-sync').create();
 
 // Scripts task
 // Uglifies javascript files
 gulp.task('scripts', function() {
 	gulp.src('./src/js/**/*.js')
 		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(concat('bundle.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('./build/js'));
 });	
@@ -33,11 +42,21 @@ gulp.task('images', function() {
 		.pipe(gulp.dest('./build/img'));
 });
 
+// Browser Sync
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			proxy: 'localhost:3000'
+		}	
+	});
+});
+
 // Watch task
-gulp.task('watch', function() {
+gulp.task('watch', ['browser-sync'], function() {
 	gulp.watch('./src/js/**/*.js', ['scripts']);
 	gulp.watch('./src/sass/**/*.scss', ['styles']);
 	gulp.watch('./src/img/*', ['images']);
+	gulp.watch(['*.html', './src/js/**/*.js', './src/sass/**/*.scss']).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['scripts', 'styles', 'images', 'watch']);
